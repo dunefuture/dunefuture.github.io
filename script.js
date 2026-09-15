a93258309e4c4b2e8ecd1fe4a21cc1d6587feb1a
dunefuture <ninjaheroeserver34@gmail.com>
2026-09-15 14:37:15 +0700

    Initial Dune Future website

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+function missingInvite(){
+  alert('Add your Discord invite link to the JOIN DISCORD button in index.html first.');
+  return false;
+}
